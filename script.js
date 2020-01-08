abc688d019f9f211aa27985bbea91e608943be3c
Irena Jurasek <irenaskydive@gmail.com>
2020-01-08 16:38:39 +0100

    add hue and line width change

--- script.js
+++ script.js
@@ -15,10 +15,14 @@ ctx.lineWidth = 50;
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
     if(!isDrawing) return; // dont run when not mouse down
     console.log(e);
+    // every 360 draws increment the color
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`; 
     // start a path
     ctx.beginPath();
     // start from
@@ -28,6 +32,25 @@ function draw(e) {
     ctx.stroke();
     // update lastX & lastY 
     [lastX, lastY] = [e.offsetX, e.offsetY];
+    // increment hue value for each draw
+    hue++;
+    // reset the hue
+    if (hue >= 360) {
+        hue = 0;
+    }
+    // if lineWidth is greater or equal to 50 or less or equal to 1
+    // flip direction
+    if (ctx.lineWidth >= 50 || ctx.lineWidth <= 1) {
+        direction = !direction;
+    }
+    // start with incrementing from 0 to 50
+    // when over 50 decrement lineWidth
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+    
 }
 
 canvas.addEventListener('mousedown', (e) => {
